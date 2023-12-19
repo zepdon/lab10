148b03472b16ca5956558b5f31d6bc96fda05409
Poon <zeepengineer@gmail.com>
2023-12-19 16:47:04 +0700

    lab10_3 completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,44 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+
+using namespace std;
+
+
+
+int main(){
+    const int MAX=100;
+    double numbers[MAX];
+    double sum = 0;
+    int count = 0;
+
+    ifstream source;
+    source.open("score.txt");
+    double number;
+    while (source >> number)
+    {
+        numbers[count] = number;
+        sum += numbers[count];
+        count++;
+    }
+    source.close();
+
+    double mean = sum / count;
+    double sumSquaredDiff = 0;
+
+    for (int i = 0; i < count; i++) {
+        sumSquaredDiff += pow(numbers[i] - mean, 2);
+    }
+
+    double standardDeviation = sqrt(sumSquaredDiff / count);
+
+
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3) ;
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << standardDeviation ;
+
+    return 0;
+}
