fdf574e7781eeb75ca2483eee47ba0918fe288e2
Poon <zeepengineer@gmail.com>
2023-12-19 16:00:23 +0700

    lab10_2 completed

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -9,8 +9,13 @@ int main (){
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	string textline;
+	dest << "-------------------- BOOM ---------------------\n";
+	while (getline(source,textline))
+	{
+		dest << textline <<'\n';
+	}
+	dest << "-------------------- HA!! ---------------------";
 
     source.close();
     dest.close();
